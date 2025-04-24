d4fc6c333529b8a031bf3e7beb570da0c00e9220
ArifRTzy <arifr0751@gmail.com>
2025-04-24 20:32:24 +0700

    feat: add about

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero"
+import About from "@/components/about"
 
 const Home = () => {
   return (
     <div className="font-inter bg-body">
       <Hero/>
+      <About/>
     </div>
   )
 }
